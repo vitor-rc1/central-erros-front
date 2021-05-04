4703ca6068f2d837e4e43c7a1a909e7b1309b820
PedroMarqDev <PedroMarqDev@gmail.com>
2021-05-04 14:18:41 -0300

    Adicionado as transiçoes e detalhes esteticos

--- src/pages/MakeTheirTomorrow.jsx
+++ src/pages/MakeTheirTomorrow.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function MakeTheirTomorrow() {
+  const [falseLoading, setFalseLoading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setFalseLoading(true), 1500);
+  });
+  if (falseLoading) return <Redirect to="/login" />;
   return (
     <div className="main-tomorrow">
       <h1>
