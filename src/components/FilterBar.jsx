e6be08fd84e4ba108a64ef46312c1e9f20ec2fd3
PedroMarqDev <PedroMarqDev@gmail.com>
2021-05-12 21:56:38 -0300

    filterBar

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import * as Actions from '../actions/index';
+
+function FilterBar() {
+  const [columnFilter, setColumnFilter] = useState('');
+  const [filterText, setFilterText] = useState('');
+  const Authorization = localStorage.getItem('Authorization') || '';
+  const dispatch = useDispatch();
+  const fetchFilter = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://centraldeerrosjava.herokuapp.com/loggers?filter=${columnFilter}&value=${filterText}`,
+      {
+        method: 'GET',
+        headers: {
+          authorization: Authorization,
+        },
+      },
+      [],
+    )
+      .then((resolve) => {
+        if (resolve.status === 500) throw new Error('Enum error!?');
+        return resolve.json();
+      })
+      .then((json) => {
+        console.log(json);
+        dispatch(Actions.storageAllLoggers(json));
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <form onSubmit={fetchFilter}>
+      <label htmlFor="text">
+        <input
+          onChange={({ target }) => setFilterText(target.value)}
+          id="text"
+          type="text"
+        />
+      </label>
+      <label htmlFor="filter">
+        <select
+          id="filter"
+          value={columnFilter}
+          onChange={({ target }) => setColumnFilter(target.value)}
+        >
+          <option value="date">Data</option>
+          <option value="description">Descrição</option>
+          <option value="source">Origem</option>
+          <option value="quantity">Quantidade</option>
+          <option value="level">Level</option>
+        </select>
+      </label>
+      <button type="submit">Pesquisar</button>
+    </form>
+  );
+}
+
+export default FilterBar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import * as Actions from '../actions/index';
+import FilterBar from '../components/FilterBar';
 import LogList from '../components/LogList';
 import PopUpLog from '../components/PopUpLog';
 import { isAuthenticated } from '../service/Auth';
@@ -9,16 +11,15 @@ import MakeTheirTomorrow from './MakeTheirTomorrowLoading';
 
 function Dashboard() {
   const [loading, setLoading] = useState(true);
-  const [loggers, setLoggers] = useState([]);
   const [redirect, setRedirect] = useState(false);
-  const { loading: popLoading, viewLog } = useSelector(
+  const { loading: popLoading, viewLog, allLoggers } = useSelector(
     (state) => state.loggers,
   );
-  console.log(popLoading);
+  const dispatch = useDispatch();
   useEffect(async () => {
     const authenticated = await isAuthenticated();
     if (authenticated[1]) {
-      setLoggers(authenticated[0]);
+      dispatch(Actions.storageAllLoggers(authenticated[0]));
       setRedirect(!authenticated[1]);
       return setLoading(false);
     }
@@ -30,7 +31,8 @@ function Dashboard() {
   return (
     <div className="main-container">
       <Header />
-      <LogList loggers={loggers} />
+      <FilterBar />
+      <LogList loggers={allLoggers} />
       {viewLog[0] && <PopUpLog log={viewLog[1]} loading={popLoading} />}
     </div>
   );
